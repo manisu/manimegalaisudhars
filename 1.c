5e74cadb8df85b23ed1da89275522c3c66553e1a
manisu <35994839+manisu@users.noreply.github.com>
2018-03-14 16:03:24 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+int a[10],b[10];
+printf("\n enter the string");
+scanf("%s",a);
+strrev(a);
+printf("\n the reversed string is ");
+printf("%s",a);
+getch();
+}
